95aa3def0fc656f1b2b86f0969510a0cd152fc3f
Get GUI <raviverman@outlook.com>
2022-05-03 22:58:10 +0530

    script: Added a dummy install script

---
+++ scripts/install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+#> [1/10] Cloning the repository
+echo "Cloning the repository"
+sleep 5
+#> [2/10] Aquiring dependencies
+echo "Aquiring dependencies"
+sleep 2
+#> [3/10] Building the code
+echo "Building the code"
+sleep 10
+#> [4/10] Integrating chips on RAM module
+echo "Integrating chips on RAM module"
+sleep 2
+#> [5/10] Writing device firmware
+echo "Writing device firmware"
+sleep 2
+#> [6/10] Adding RAM module into the slot
+echo "Adding RAM module into the slot"
+sleep 3
+#> [7/10] Reseating the RAM modules properly
+echo "Reseating the RAM modules properly"
+sleep 5
+#> [8/10] Resetting RAM configuration files
+echo "Resetting RAM configuration files"
+sleep 1
+#> [9/10] Adding environment variables
+echo "Adding environment variables"
+sleep 1
+#> [10/10] Finishing up..
+echo "Finishing up"
+sleep 4
+echo "Done"
